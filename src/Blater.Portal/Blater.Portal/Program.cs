9d5fe0b494dc23783f1d6b10ee6df66bb245df1f
breno <breno@autosystem.top>
2024-05-14 17:05:13 -0300

    start project

--- src/Blater.Portal/Blater.Portal/Program.cs
+++ src/Blater.Portal/Blater.Portal/Program.cs
@@ -1,4 +1,5 @@
-using Blater.Portal.Client.Components;
+using Blater.Portal.Apps;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
