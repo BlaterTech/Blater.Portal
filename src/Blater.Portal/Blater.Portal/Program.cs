c725514d298edfdafab5e426430017454767c5af
breno <breno@autosystem.top>
2024-05-15 17:06:18 -0300

    continue development blater portal

--- src/Blater.Portal/Blater.Portal/Program.cs
+++ src/Blater.Portal/Blater.Portal/Program.cs
@@ -3,35 +3,33 @@ using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMudServices();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+       .AddInteractiveServerComponents()
+       .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddMudServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+    app.UseExceptionHandler("/Error", true);
+    app.UseHsts();
 }
 else
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseWebAssemblyDebugging();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Blater.Portal.Client._Imports).Assembly);
+   .AddInteractiveServerRenderMode()
+   .AddInteractiveWebAssemblyRenderMode()
+   .AddAdditionalAssemblies(typeof(Blater.Portal.Client._Imports).Assembly);
 
 await app.RunAsync();
