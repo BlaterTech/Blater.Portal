108530b94d111a1131ffa6b7a9cc01fda0fb4ba5
breno <breno@autosystem.top>
2024-05-16 16:56:44 -0300

    adjusting breakpoint in grids

--- src/Blater.Portal/Blater.Portal.Client/Pages/Home.razor.cs
+++ src/Blater.Portal/Blater.Portal.Client/Pages/Home.razor.cs
@@ -1,41 +1,141 @@
-﻿using MudBlazor;
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Blater.Portal.Client.Pages;
 
 public partial class Home
 {
+    [Inject]
+    protected IBrowserViewportService BrowserViewportService { get; set; } = null!;
+    
     private Typo _title = Typo.h1;
     private Typo _subTitle = Typo.h2;
-    private string _firstGridStyle = "";
-    private void OnBreakpointCallback(Breakpoint obj)
+    private Dictionary<int, Dictionary<Breakpoint, string>> _dictGridBreakpoint = new();
+    private readonly Dictionary<int, string> _dictGridStyle = new();
+    
+    private void InitializedDictGridBreakpoint()
     {
-        int i = 1;
+        _dictGridBreakpoint = new Dictionary<int, Dictionary<Breakpoint, string>>
+        {
+            {
+                1, new Dictionary<Breakpoint, string>
+                {
+                    { Breakpoint.Xs, "height: 70vh" },
+                    { Breakpoint.Sm, "height: 60vh" },
+                    { Breakpoint.Md, "height: 60vh" },
+                    { Breakpoint.Lg, "height: 95vh" },
+                    { Breakpoint.Xl, "height: 95vh" },
+                    { Breakpoint.Xxl, "height: 95vh" },
+                }
+            },
+            {
+                2, new Dictionary<Breakpoint, string>
+                {
+                    { Breakpoint.Xs, "height: 150vh" },
+                    { Breakpoint.Sm, "height: 150vh" },
+                    { Breakpoint.Md, "height: 110vh" },
+                    { Breakpoint.Lg, "height: 110vh" },
+                    { Breakpoint.Xl, "height: 110vh" },
+                    { Breakpoint.Xxl, "height: 110vh" },
+                }
+            },
+            {
+                3, new Dictionary<Breakpoint, string>
+                {
+                    { Breakpoint.Xs, "height: 65vh" },
+                    { Breakpoint.Sm, "height: 65vh" },
+                    { Breakpoint.Md, "height: 65vh" },
+                    { Breakpoint.Lg, "height: 65vh" },
+                    { Breakpoint.Xl, "height: 65vh" },
+                    { Breakpoint.Xxl, "height: 65vh" },
+                }
+            },
+            {
+                4, new Dictionary<Breakpoint, string>
+                {
+                    { Breakpoint.Xs, "height: 111vh" },
+                    { Breakpoint.Sm, "height: 111vh" },
+                    { Breakpoint.Md, "height: 55vh" },
+                    { Breakpoint.Lg, "height: 30vh" },
+                    { Breakpoint.Xl, "height: 30vh" },
+                    { Breakpoint.Xxl, "height: 30vh" },
+                }
+            },
+            {
+                5, new Dictionary<Breakpoint, string>
+                {
+                    { Breakpoint.Xs, "height: 75vh" },
+                    { Breakpoint.Sm, "height: 75vh" },
+                    { Breakpoint.Md, "height: 85vh" },
+                    { Breakpoint.Lg, "height: 25vh" },
+                    { Breakpoint.Xl, "height: 25vh" },
+                    { Breakpoint.Xxl, "height: 25vh" },
+                }
+            }
+        };
+    }
+    
+    private void UpdateGrid(int key, Breakpoint obj)
+    {
+        if (!_dictGridBreakpoint.TryGetValue(key, out var value))
+        {
+            return;
+        }
+        
+        if (!value.TryGetValue(obj, out var gridStyleValue))
+        {
+            return;
+        }
+        
+        if (!_dictGridStyle.TryAdd(key, gridStyleValue))
+        {
+            _dictGridStyle[key] = gridStyleValue;
+        }
+    }
+    
+    protected override async Task OnInitializedAsync()
+    {
+        InitializedDictGridBreakpoint();
+        
+        var currentBreakpoint = await BrowserViewportService.GetCurrentBreakpointAsync().ConfigureAwait(false);
+        for (var i = 0; i <= 5; i++)
+        {
+            UpdateGrid(i, currentBreakpoint);
+        }
+        
+        await InvokeAsync(StateHasChanged);
+    }
+    
+    private void UpdateFonts(Typo title, Typo subTitle)
+    {
+        _title = title;
+        _subTitle = subTitle;
+    }
+    
+    private void OnBreakpointCallback(int value, Breakpoint obj)
+    {
         switch (obj)
         {
             case Breakpoint.Xs:
-                _title = Typo.h4;
-                _subTitle = Typo.h5;
-                _firstGridStyle = "height: 70vh";
+                UpdateFonts(Typo.h4, Typo.h5);
+                UpdateGrid(value, obj);
                 break;
             case Breakpoint.Sm:
-                _title = Typo.h3;
-                _subTitle = Typo.h4;
-                _firstGridStyle = "height: 50vh";
-                break;
             case Breakpoint.Md:
-                _title = Typo.h3;
-                _subTitle = Typo.h4;
-                _firstGridStyle = "height: 60vh";
+                UpdateFonts(Typo.h3, Typo.h4);
+                UpdateGrid(value, obj);
                 break;
             case Breakpoint.Lg:
             case Breakpoint.Xl:
             case Breakpoint.Xxl:
-                _title = Typo.h2;
-                _subTitle = Typo.h3;
-                _firstGridStyle = "height: 95vh";
+                UpdateFonts(Typo.h2, Typo.h3);
+                UpdateGrid(value, obj);
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(obj), obj, null);
         }
+        
+        StateHasChanged();
     }
+    
 }
