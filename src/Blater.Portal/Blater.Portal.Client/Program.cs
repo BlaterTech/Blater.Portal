a43401719acabe7c510b6bf3c30e661e61760aa5
breno <breno@autosystem.top>
2024-06-26 10:45:54 -0300

    ✨ feat(api): integrate Blater SDK and add auth services #1234
    - Added Blater SDK references and authentication services.
    - Updated target framework to .NET 9.0.
    
    Benefits:
    - Improved authentication handling.
    - Enhanced code reusability.

--- src/Blater.Portal/Blater.Portal.Client/Program.cs
+++ src/Blater.Portal/Blater.Portal.Client/Program.cs
@@ -1,12 +1,29 @@
+using Blater.Interfaces.BlaterAuthentication.Repositories;
 using Blater.Portal.Client.Services;
+using Blater.SDK.Implementations.BlaterAuthentication.Repositories;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using IBlaterAuthEmailRepository = Blater.SDK.Interfaces.IBlaterAuthEmailRepository;
+using IBlaterAuthLoginRepository = Blater.SDK.Interfaces.IBlaterAuthLoginRepository;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<BrowserViewportObserverService>();
+
+#pragma warning disable CA2252
+builder.Services.AddScoped<IBlaterAuthEmailRepository, BlaterAuthEmailRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthLoginRepository, BlaterAuthLoginRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthLockoutRepository, BlaterAuthLockoutRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthTwoFactorRepository, BlaterAuthTwoFactorRepositoryEndPoints>();
+//builder.Services.AddScoped<IBlaterAuthPasswordRepository, BlaterAuthPasswordRepository>();
+builder.Services.AddScoped<IBlaterAuthPermissionRoleRepository, BlaterAuthPermissionRoleRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthPermissionRepository, BlaterAuthPermissionRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthRoleRepository, BlaterAuthRoleRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthUserRoleRepository, BlaterAuthUserRoleRepositoryEndPoints>();
+builder.Services.AddScoped<IBlaterAuthUserPermissionRepository, BlaterAuthUserPermissionsRepositoryEndPoints>();
+#pragma warning restore CA2252
 
 var app = builder.Build();
 
--- src/Blater.Portal/Blater.Portal/Program.cs
+++ src/Blater.Portal/Blater.Portal/Program.cs
@@ -1,6 +1,17 @@
+using System.Net.Http.Headers;
+using System.Text;
+using Blater;
+using Blater.Exceptions;
+using Blater.Interfaces;
+using Blater.Interfaces.BlaterAuthentication.Stores;
 using Blater.Portal.Apps;
 using Blater.Portal.Client.Services;
+using Blater.SDK.Implementations.BlaterAuthentication.Stores;
+using Blater.SDK.Implementations.BlaterDatabase.Stores;
+using Blater.SDK.Implementations.BlaterKeyValue.Stores;
 using MudBlazor.Services;
+using IBlaterAuthEmailStore = Blater.SDK.Interfaces.IBlaterAuthEmailStore;
+using IBlaterAuthLoginStore = Blater.SDK.Interfaces.IBlaterAuthLoginStore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +23,40 @@ builder.Services.AddRazorComponents()
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<BrowserViewportObserverService>();
+
+builder.Services.AddHttpClient<BlaterHttpClient>((services, client) =>
+{
+    var connectionString = services
+                          .GetRequiredService<IConfiguration>()
+                          .GetConnectionString("CouchDB");
+
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+        throw new BlaterException("ConnectionString is nullable");
+    }
+            
+    client.BaseAddress = new Uri(connectionString);
+
+    const string userName = "test";
+    const string userPassword = "test";
+    const string authenticationString = $"{userName}:{userPassword}";
+    var base64String = Convert.ToBase64String(Encoding.ASCII.GetBytes(authenticationString));
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64String);
+});
+
+builder.Services.AddScoped<IBlaterDatabaseStore, BlaterDatabaseStoreEndPoints>();
+builder.Services.AddScoped<IBlaterKeyValueStore, BlaterKeyValueStoreEndPoints>();
+builder.Services.AddScoped(typeof(IBlaterDatabaseStoreT<>), typeof(BlaterDatabaseStoreTEndPoints<>));
+builder.Services.AddScoped<IBlaterAuthEmailStore, BlaterAuthEmailStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthLoginStore, BlaterAuthLoginStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthLockoutStore, BlaterAuthLockoutStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthTwoFactorStore, BlaterAuthTwoFactorStoreEndPoints>();
+//builder.Services.AddScoped<IBlaterAuthPasswordStore, password>();
+builder.Services.AddScoped<IBlaterAuthPermissionRoleStore, BlaterAuthPermissionRoleStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthPermissionStore, BlaterAuthPermissionStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthRoleStore, BlaterAuthRoleStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthUserRoleStore, BlaterAuthUserRoleStoreEndPoints>();
+builder.Services.AddScoped<IBlaterAuthUserPermissionStore, BlaterAuthUserPermissionStoreEndPoints>();
 
 var app = builder.Build();
 
