67d248846ee7ff3a85847c930461c9d615b3d5be
breno <breno@autosystem.top>
2024-06-27 13:12:21 -0300

    ✨ feat(client): enhance login and service configurations #123
    Added new service using directives in BlaterLogin.razor and updated
    Program.cs for better service configurations and exception handling.
    
    Benefits:
    - Improved service initialization
    - Enhanced error handling
    - More flexible Snackbar configurations
    
    Closes #123

--- src/Blater.Portal/Blater.Portal.Client/Program.cs
+++ src/Blater.Portal/Blater.Portal.Client/Program.cs
@@ -1,3 +1,4 @@
+using Blater.Frontend.Services;
 using Blater.Models.User;
 using Blater.Portal.Client.Services;
 using Blater.SDK.Extensions;
@@ -5,6 +6,7 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
+#pragma warning disable CA2252
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
@@ -16,14 +18,11 @@ builder.Services.AddBlazoredLocalStorage(config =>
     config.JsonSerializerOptions.WriteIndented = true;
 });
 
-#pragma warning disable CA2252
-
 builder.Services.AddScoped<BlaterUserToken>();
 builder.Services.AddScoped<AuthenticationService>();
 builder.Services.AddScoped<NavigationService>();
 
 builder.Services.AddBlaterServices();
-#pragma warning restore CA2252
 
 var app = builder.Build();
 
@@ -35,3 +34,5 @@ finally
 {
     await app.DisposeAsync();
 }
+
+#pragma warning restore CA2252
--- src/Blater.Portal/Blater.Portal/Program.cs
+++ src/Blater.Portal/Blater.Portal/Program.cs
@@ -1,37 +1,74 @@
+using Blater.Frontend.Auto;
+using Blater.Frontend.Logging;
+using Blater.Frontend.Services;
 using Blater.Models.User;
 using Blater.Portal.Apps;
-using Blater.Portal.Client.Services;
 using Blater.SDK.Extensions;
 using Blazored.LocalStorage;
+using Microsoft.IdentityModel.Logging;
+using MudBlazor;
 using MudBlazor.Services;
 
+#pragma warning disable CA2252
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<BrowserViewportObserverService>();
+builder.Services.Configure<HostOptions>(hostOptions =>
+{
+    hostOptions.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
+});
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-       .AddInteractiveServerComponents()
-       .AddInteractiveWebAssemblyComponents();
+IdentityModelEventSource.ShowPII = true;
 
-builder.Services.AddMudServices();
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddResponseCompression();
+}
 
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+    config.SnackbarConfiguration.PreventDuplicates = true;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+        
 builder.Services.AddBlazoredLocalStorage(config =>
 {
     config.JsonSerializerOptions.WriteIndented = true;
 });
+        
+//Scoped services (AKA per user)
 
-#pragma warning disable CA2252
 builder.Services.AddScoped<BlaterUserToken>();
+
 builder.Services.AddScoped<AuthenticationService>();
 builder.Services.AddScoped<NavigationService>();
 
+builder.Services.AddSingleton<LocalizationService>();
+        
 builder.Services.AddBlaterServices();
-#pragma warning restore CA2252
+
+builder.Services
+       .AddRazorComponents()
+       .AddInteractiveServerComponents()
+       .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+AutoComponentsBuilders.Initialize();
+        
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+
+app.UseStatusCodePagesWithRedirects("/Error/{0}");
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", true);
@@ -42,12 +79,6 @@ else
     app.UseWebAssemblyDebugging();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode()
    .AddInteractiveWebAssemblyRenderMode()
@@ -55,10 +86,11 @@ app.MapRazorComponents<App>()
 
 try
 {
-    await app.RunAsync();
-    await app.StopAsync();
+    await app.StartAsync();
+    await app.WaitForShutdownAsync();
 }
 finally
 {
     await app.DisposeAsync();
 }
+#pragma warning restore CA2252
