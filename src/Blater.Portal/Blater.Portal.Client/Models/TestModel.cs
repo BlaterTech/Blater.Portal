8fd355ec909cd5dffc02102a58360dd914999bf3
breno <breno@autosystem.top>
2024-06-28 18:05:54 -0300

    ✨ feat(frontend): integrate MudBlazor icons across components #123
    Added `@using Icons = MudBlazor.Icons` to various components for UI consistency.
    
    - Enhanced visual appeal and standardization across the app.
    - Simplified access to MudBlazor icons in multiple components.
    
    ---
    
    ✨ feat(client): create TestModel with CRUD operations #124
    
    Implemented `TestModel` and corresponding CRUD page `TestCRUD.razor`.
    
    - Streamlined data management with AutoDataGrid integration.
    - Improved maintainability with a centralized data model.
    
    ---
    
    🔧 chore: update project references and dependencies #125
    
    Removed unnecessary internal references and updated package references.
    
    - Reduced project complexity.
    - Ensured compatibility with the latest dependency versions.

---
+++ src/Blater.Portal/Blater.Portal.Client/Models/TestModel.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Blater.Attributes.Auto;
+using Blater.AutoModelConfigurations;
+using Blater.Enumerations;
+using Blater.Enumerations.AutoModel;
+using Blater.Models.Bases;
+
+namespace Blater.Portal.Client.Models;
+
+[SuppressMessage("Usage", "CA2252:Esta API requer a aceitação de recursos de visualização")]
+[AutoPage(false, true, true, true, BlaterProjects.Blater)]
+public class TestModel : BaseFrontendModel
+{
+    [Required]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public decimal Price { get; set; }
+
+    [Required]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    public int Stock { get; set; }
+
+    public bool IsInSale { get; set; }
+
+    [Required]
+    public DateTime SaleStartDate { get; set; }
+
+    [Required]
+    public DateTime SaleEndDate { get; set; }
+    
+    public bool Featured { get; set; }
+    
+    public override void Configure(AutoModelConfigurator configurator)
+    {
+        configurator.Property(() => Name)
+                    .TableDisplayType(AutoComponentType.AutoText)
+                    .FormDisplayType(AutoFormInputType.AutoTextInput)
+                    .DetailsDisplayType(AutoComponentType.AutoText)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => Price)
+                    .TableDisplayType(AutoComponentType.AutoMoneyRoundedTable)
+                    .DetailsDisplayType(AutoComponentType.AutoMoneyRounded)
+                    .FormDisplayType(AutoFormInputType.AutoDecimalInput)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => Description)
+                    .FormDisplayType(AutoFormInputType.AutoTextAreaInput)
+                    .DetailsDisplayType(AutoComponentType.AutoTextArea)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => Stock)
+                    .TableDisplayType(AutoComponentType.AutoNumeric)
+                    .FormDisplayType(AutoFormInputType.AutoNumericInput)
+                    .DetailsDisplayType(AutoComponentType.AutoNumeric)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => SaleStartDate)
+                    .FormDisplayType(AutoFormInputType.AutoDateTimeInput)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => SaleEndDate)
+                    .FormDisplayType(AutoFormInputType.AutoDateTimeInput)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => IsInSale)
+                    .FormDisplayType(AutoFormInputType.AutoSwitchInput)
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => Featured)
+                    .FormDisplayType(AutoFormInputType.AutoSwitchInput)
+                    .DetailsDisplayType(AutoComponentType.AutoTextStatus)
+                    .AddExtraAttribute("TypeName", "AutoTextStatus-StoreProduct-Featured")
+                    .GridType(AutoGridType.FullWidth);
+        
+        configurator.Property(() => CreatedAt)
+                    .TableDisplayType(AutoComponentType.AutoDate);
+    }
+}
---
+++ src/Blater.Portal/Blater.Portal.Client/Pages/Portal/TestCRUD.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Blater.Portal.Client.Pages.Portal;
+
+[SuppressMessage("Usage", "CA2252:Esta API requer a aceitação de recursos de visualização")]
+public partial class TestCRUD
+{
+    
+}
