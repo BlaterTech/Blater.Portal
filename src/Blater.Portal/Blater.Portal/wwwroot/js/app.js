8060e8525ae6fde214e324334b63cdb47e4b7212
breno <breno@autosystem.top>
2024-07-26 15:36:01 -0300

    ✨ feat(client): enhance code editor and update links #123
    Improved code editor with copy functionality and updated footer links
    
    - Added copy button to BlaterCodeEditor for easy text copying
    - Enhanced BlaterLandingFooter links to point to actual pages
    - Included BlazorMonaco package for advanced code editing
    - Added custom JavaScript for clipboard operations
    
    These changes improve the user experience by making code interactions more intuitive and ensuring all footer links are functional.

---
+++ src/Blater.Portal/Blater.Portal/wwwroot/js/app.js
@@ -0,0 +1,7 @@
+﻿function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Texto copiado para a área de transferência.');
+    }).catch(function(err) {
+        console.error('Erro ao copiar o texto: ', err);
+    });
+}
