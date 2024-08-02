f9989c154ddd288df744beedb50ed6c51ec96c85
breno <breno@autosystem.top>
2024-08-02 17:25:26 -0300

    ✨ feat(auto-table): integrate AutoTable with new configurations #42
    Added AutoTable initialization in WebSetup.cs and created new classes
    for table and column configurations. Enhanced BlaterTable component to
    use dynamic column configurations, improving flexibility for table
    rendering.
    
    This improves maintainability by centralizing table configs and reduces
    boilerplate code needed for table setup.

---
+++ src/Blater.Portal/Blater.Portal.Client/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Blater.Models.Bases;
+
+namespace Blater.Portal.Client.Models;
+
+public class Employee : BaseDataModel
+{
+    public string? Name { get; set; }
+    public string? Position { get; set; }
+    public int YearsEmployed { get; set; }
+    public int Salary { get; set; }
+    public int Rating { get; set; }
+}
---
+++ src/Blater.Portal/Blater.Portal.Client/TableConfiguration/EmployeeTableConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Blater.Frontend.Client.Auto.AutoTable.Implementations;
+using Blater.Frontend.Client.Auto.AutoTable.Interfaces;
+using Blater.Portal.Client.Models;
+
+namespace Blater.Portal.Client.TableConfiguration;
+
+public class EmployeeTableConfiguration : ITableConfiguration<Employee>
+{
+    public void Configure(TableConfigurationBuilder<Employee> builder)
+    {
+        builder
+           .ToTable("Employees");
+
+        builder
+           .Property(x => x.Name)
+           .Order(1);
+        builder
+           .Property(x => x.Position)
+           .Order(2);
+        builder
+           .Property(x => x.Rating)
+           .Order(3);
+        builder
+           .Property(x => x.Salary)
+           .Order(4);
+        builder
+           .Property(x => x.YearsEmployed)
+           .Order(5);
+
+        builder
+           .Property(x => x.Id)
+           .DisabledColumn();
+        builder
+           .Property(x => x.CreatedAt)
+           .DisabledColumn();
+        builder
+           .Property(x => x.UpdatedAt)
+           .DisabledColumn();
+        builder
+           .Property(x => x.Enabled)
+           .DisabledColumn();
+        builder
+           .Property(x => x.Deleted)
+           .DisabledColumn();
+    }
+}
