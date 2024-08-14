b08483bed167a05ce05acec8ac0f2bb6dba8f7fb
breno <breno@autosystem.top>
2024-08-14 14:52:13 -0300

    ✨ feat(auto): simplify IAutoTableConfigurationBuilder and IAutoColumnConfigurationBuilder interfaces
    Refactored interfaces to consolidate generic types and improve code clarity.
    This change reduces redundancy and simplifies the method signatures.
    
    Benefits:
    - Improved code readability
    - Easier maintenance and extension of interfaces

--- src/Blater.Portal/Blater.Portal.Client/Configurations/TableConfiguration/EmployeeAutoTableConfiguration.cs
+++ src/Blater.Portal/Blater.Portal.Client/Configurations/TableConfiguration/EmployeeAutoTableConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Blater.Frontend.Client.Auto.AutoBuilders.Table;
+﻿using System.Linq.Expressions;
+using Blater.Frontend.Client.Auto.AutoBuilders.Table;
 using Blater.Frontend.Client.Auto.Interfaces.AutoTable;
 using Blater.Portal.Client.Models;
 
@@ -9,25 +10,25 @@ public class EmployeeAutoTableConfiguration : IAutoTableConfiguration<Employee>
     public void Configure(AutoTableConfigurationBuilder<Employee> builder)
     {
         builder.Table("Employees");
-        
+
         builder.Column(x => x.Position, configurationBuilder =>
         {
             configurationBuilder.Name("");
             configurationBuilder.Order(1);
         });
-        
+
         builder.Column(x => x.Position, configurationBuilder =>
         {
             configurationBuilder.Name("");
             configurationBuilder.Order(1);
         });
-        
+
         builder.Column(x => x.Position, configurationBuilder =>
         {
             configurationBuilder.Name("");
             configurationBuilder.Order(1);
         });
-        
+
         builder.Column(x => x.Position, configurationBuilder =>
         {
             configurationBuilder.Name("");
