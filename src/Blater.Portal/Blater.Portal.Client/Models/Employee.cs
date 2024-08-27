d1b31fe09f85f4bbbc96537e92305f729eb4a54b
breno <breno@autosystem.top>
2024-08-27 15:36:48 -0300

    ✨ feat(auto): refactor AutoComponentConfigurationBuilder to support non-generic configurations
    - Removed generic constraints from AutoComponentConfigurationBuilder.
    - Introduced AutoModelConfiguration for streamlined model handling.
    - Updated dependent classes and methods to use new configuration approach.
    
    Benefits: Simplifies the configuration process and improves code readability.

--- src/Blater.Portal/Blater.Portal.Client/Models/Employee.cs
+++ src/Blater.Portal/Blater.Portal.Client/Models/Employee.cs
@@ -1,12 +1,69 @@
-﻿using Blater.Models.Bases;
+﻿using Blater.Frontend.Client.Auto.AutoBuilders;
+using Blater.Frontend.Client.Enumerations;
+using Blater.Frontend.Client.Models.Bases;
+using Blater.Models.Bases;
+using FluentValidation;
+using MudBlazor;
 
 namespace Blater.Portal.Client.Models;
 
-public class Employee : BaseDataModel
+public class Employee : BaseFrontendModel
 {
     public string? Name { get; set; }
     public string? Position { get; set; }
     public int YearsEmployed { get; set; }
     public int Salary { get; set; }
     public int Rating { get; set; }
+
+
+    public override void Configure(AutoComponentConfigurationBuilder builder)
+    {
+        builder.Table("TableName", configurationBuilder =>
+        {
+            configurationBuilder.AddMember(() => Salary);
+        });
+
+        builder.Form("FormName", configurationBuilder =>
+        {
+            configurationBuilder
+               .AddMember(() => Name)
+               .Validate(initial => initial.NotEmpty())
+               .Order(1);
+
+            configurationBuilder.AddGroup("GroupName", AutoFormGroupScope.Create, groupConfigurationBuilder =>
+            {
+                groupConfigurationBuilder
+                   .AddMember(() => Name)
+                   .LabelName("");
+                groupConfigurationBuilder
+                   .AddMember(() => Name)
+                   .LabelName("");
+                groupConfigurationBuilder
+                   .AddMember(() => Name)
+                   .LabelName("");
+            });
+        });
+
+        builder.Details("DetailsName", configurationBuilder =>
+        {
+            configurationBuilder
+               .AddMember(() => Name)
+               .Breakpoint(Breakpoint.Lg, 12);
+
+            configurationBuilder.AddGroup("GroupName", false, groupConfigurationBuilder =>
+            {
+                groupConfigurationBuilder
+                   .AddMember(() => Name)
+                   .ComponentType("");
+
+                groupConfigurationBuilder
+                   .AddMember(() => Name)
+                   .ComponentType("");
+
+                groupConfigurationBuilder
+                   .AddMember(() => Name)
+                   .ComponentType("");
+            });
+        });
+    }
 }
