b2fce5f1f970ebb1c9dfe640bfe98d01ee75ee26
mariogkds <mariogk@blokods.com.br>
2024-09-03 18:59:12 +0100

    Segura ai brenaum

--- src/Blater.Portal/Blater.Portal.Client/Models/Employee.cs
+++ src/Blater.Portal/Blater.Portal.Client/Models/Employee.cs
@@ -1,28 +1,45 @@
-﻿using System.Linq.Expressions;
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using Blater.Frontend.Client.Auto.AutoBuilders;
+using Blater.Frontend.Client.Auto.AutoBuilders.Form;
+using Blater.Frontend.Client.Auto.AutoBuilders.Table;
+using Blater.Frontend.Client.Auto.AutoModels.Form;
+using Blater.Frontend.Client.Auto.Interfaces.Types;
+using Blater.Frontend.Client.Models;
 using Blater.Frontend.Client.Models.Bases;
 using Blater.Frontend.Client.Services;
 using FluentValidation;
+using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace Blater.Portal.Client.Models;
 
-public class Employee : BaseFrontendModel<Employee>
+public class Employee : BaseFrontendModel, IAutoTable<Employee>, IAutoForm<Employee>
 {
+    
     public string Name { get; set; } = null!;
+    
     public string Position { get; set; } = null!;
     public int YearsEmployed { get; set; }
     public int Salary { get; set; }
     public int Rating { get; set; }
 
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+    
+    private AutoFormActionConfiguration _formActionConfiguration = new();
+
     public override void Configure(AutoModelConfigurationBuilder<Employee> builder)
     {
-        builder.Form("FormName", configurationBuilder =>
+        _formActionConfiguration = new AutoFormActionConfiguration
         {
-            configurationBuilder.Actions(actionConfigurationBuilder =>
-            {
-                actionConfigurationBuilder.TypeCreateEditButton(ButtonType.Submit);
-            });
+            ColorCancelButton = Color.Dark,
+        };
+
+        builder.Form(_formActionConfiguration, configurationBuilder =>
+        {
+            configurationBuilder.Actions(actionConfigurationBuilder => { actionConfigurationBuilder.TypeCreateEditButton(ButtonType.Submit); });
 
             configurationBuilder.AddGroup(groupConfigurationBuilder =>
             {
@@ -32,11 +49,13 @@ public class Employee : BaseFrontendModel<Employee>
                         componentConfigurationBuilder
                            .Placeholder("Name")
                            .HelpMessage("Name")
+                           .Placeholder("asda")
                            .IsReadOnly(true)
                            .LabelName("Name")
                            .Validate(initial =>
                             {
                                 initial.NotEmpty();
+                                
                             })
                            .OnValueChanged(NameChanged);
                     });
@@ -51,6 +70,9 @@ public class Employee : BaseFrontendModel<Employee>
                            .LabelName("Position")
                            .OnValueChanged(PositionChanged);
                     });
+                
+                groupConfigurationBuilder
+                   .AddMember(() => Position, config);
             });
 
             configurationBuilder.AddGroup(groupConfigurationBuilder =>
@@ -93,10 +115,24 @@ public class Employee : BaseFrontendModel<Employee>
         });
     }
 
+    public void ConfigureTable(AutoTableMemberConfigurationBuilder builder)
+    {
+    }
+
+    public void ConfigureForm(AutoModelConfigurationBuilder<Employee> builder)
+    {
+        throw new NotImplementedException();
+    }
+
     public void NameChanged(string value)
     {
         Console.WriteLine($"NameChanged foi chamado com valor: {value}");
         Name = $"{value} + test = {value}test";
+
+        if (Name == "Xablau")
+        {
+            _formActionConfiguration.ColorCancelButton = Color.Primary;
+        }
 
         StateNotifierService.NotifyStateChanged(() => Name);
     }
